82282e3057a3f5a4b2f77dda8a5c5a0cbfdbd005
Mark Oleson <fusion2004@gmail.com>
2020-04-26 22:20:32 -0400

    Now we can set the project directory via a flag, and read .env and a config file from it

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const { Command, flags } = require('@oclif/command');
+const path = require('path');
 
 class I18NCloudTranslatorCommand extends Command {
   async run() {
     const { flags } = this.parse(I18NCloudTranslatorCommand);
-    const name = flags.name || 'world';
-    this.log(`hello ${name} from ./src/index.js`);
+
+    let dir;
+    if (flags.dir) {
+      dir = flags.dir;
+    } else {
+      dir = process.cwd();
+    }
+
+    process.env.PROJECT_DIR = dir;
+    process.env.CONFIG_FILE = path.join(dir, '.i18n-cloud-translator.json');
+    let config = require('./config');
+
+    console.log(config.toString());
   }
 }
 
@@ -18,7 +30,10 @@ I18NCloudTranslatorCommand.flags = {
   version: flags.version({ char: 'v' }),
   // add --help flag to show CLI version
   help: flags.help({ char: 'h' }),
-  name: flags.string({ char: 'n', description: 'name to print' }),
+  dir: flags.string({
+    char: 'd',
+    description: 'directory to read config and write translations to (defaults to current dir)',
+  }),
 };
 
 module.exports = I18NCloudTranslatorCommand;
